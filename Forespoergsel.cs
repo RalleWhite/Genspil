3a1752f4f3ae6e8784afbf3dd75c18dd790acf0e
CeciliaHafezan <ceciliaabel@nalaboeger.dk>
2025-04-02 11:41:24 +0200

    Tilføjet forespørgselsfunktion

--- Forespoergsel.cs
+++ Forespoergsel.cs
@@ -1,16 +1,33 @@
 using System;
 using System.Collections.Generic;
 
+public enum ForespoergselsStatus
+{
+    Afventer = 1,
+    Afsluttet = 2,
+    Annulleret = 3
+}
 internal class Forespoergsel
 {
     public int ForespoergselNr { get; set; }
     public DateTime Dato { get; set; }
-    public int Antal { get; set; }
+      
+    public string SpilNavn { get; set; }
 
-    public Forespoergsel(int forespoergselNr, DateTime dato, int antal)
+    public ForespoergselsStatus Status { get; set; }
+
+    public Kunde Kunde { get; set; }
+
+    
+
+
+    public Forespoergsel(int forespoergselNr, DateTime dato, string spilNavn, ForespoergselsStatus status, Kunde kunde)
     {
         ForespoergselNr = forespoergselNr;
-        Dato = dato;
-        Antal = antal;
-    }
+        Dato = dato.Date;
+        SpilNavn = spilNavn;
+        Status = status;
+        Kunde = kunde;
+
+     }    
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -29,7 +30,7 @@ namespace Genspil
                 switch (taskNum)
                 {
                     case 1:
-                        if (loggedIn){
+                        if (loggedIn) {
                             LagerMenu();
                             break;
                         }
@@ -177,10 +178,253 @@ namespace Genspil
 
         static void ForespoergselMenu()
         {
-            Console.WriteLine("Forespørgsler vises her");
-            Thread.Sleep(3000);
+            Console.WriteLine("1. Tilføj forespørgsel | 2. Vis forespørgsler | 3. Rediger/ slet forespørgsel | 4. Afslut");
+            if (int.TryParse(Console.ReadLine(), out int valg))
+            {
+                switch (valg)
+                {
+                    case 1:
+                        TilføjForespørgsel();
+                        break;
+                    case 2:
+                        VisForespørgsler();
+                        break;
+                    case 3:
+                        RedigerEllerSletForespoergsel();
+                        break;
+                    case 4:
+                        StartMenu();
+                        break;
+                    default:
+                        Fejlmeddelelse();
+                        break;
+
+                }
+            }
+            else
+            {
+                Fejlmeddelelse();
+
+            }
             StartMenu();
         }
+
+        static int GenererForespoergselsnummer()
+        {
+            if (forespoergsler.Count == 0) return 1;
+
+            return forespoergsler.Max(f => f.ForespoergselNr) + 1;
+            
+        }
+
+        static void TilføjForespørgsel()
+        {
+            Console.WriteLine("Tilføj en forespørgsel");
+            Console.WriteLine();
+            Console.Write("Indtast dato for forespørgsel (dd-mm-yyyy): ");
+            string inputDato = Console.ReadLine();
+            DateTime dato;
+            
+            while (!DateTime.TryParse(inputDato, out dato))
+            {
+                Console.WriteLine("Ugyldig dato. Prøv igen.");
+                inputDato = Console.ReadLine();
+            }
+            Console.Write("Indtast ønsket spil: ");
+            string spilNavn = Console.ReadLine();
+            Console.Write("Indtast kundens navn: ");
+            string kundeNavn = Console.ReadLine();
+            Console.Write("Indtast kundens telefonnummer: ");
+            string kundeTelefon = Console.ReadLine();
+            Console.Write("Indtast kundens adresse: ");
+            string kundeAdresse = Console.ReadLine();
+            Console.Write("Indtast kundens email: ");
+            string kundeEmail = Console.ReadLine();
+            Console.Write("Indtast status: 1 = Afventer, 2 = Afsluttet, 3 = Annulleret: ");
+
+            if(!int.TryParse(Console.ReadLine(), out int statusNum) || !Enum.IsDefined(typeof(ForespoergselsStatus), statusNum))
+            {
+                Console.WriteLine("Ugyldig status. Prøv igen.");
+                statusNum = (int)ForespoergselsStatus.Afventer;
+            }
+
+            ForespoergselsStatus status = (ForespoergselsStatus)statusNum;
+
+            Kunde kunde = new Kunde(kundeNavn, kundeAdresse, kundeTelefon, kundeEmail);
+
+            int forespørgselsNr = GenererForespoergselsnummer();
+            Forespoergsel forespørgsel = new Forespoergsel(forespørgselsNr, dato, spilNavn, status, kunde);
+
+            forespoergsler.Add(forespørgsel);
+
+            
+
+            Console.WriteLine("Forespørgslen blev tilføjet!");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Antal forespørgsler i listen: {forespoergsler.Count}");
+
+            Thread.Sleep(2000);
+            StartMenu();
+        }
+
+        static void VisForespørgsler()
+        { 
+            if (forespoergsler.Count == 0)
+            {
+                Console.WriteLine("Der er ingen forespørgsler");
+                return; 
+            }
+            else
+            {
+                foreach (var forespørgsel in forespoergsler)
+                {
+                    Console.WriteLine($"Forespørgsel: {forespørgsel.ForespoergselNr}");
+                    Console.WriteLine($"Dato: {forespørgsel.Dato:dd-MM-yyyy}");
+                    Console.WriteLine($"Spil: {forespørgsel.SpilNavn}");
+                    Console.WriteLine($"Kunde: {forespørgsel.Kunde.Navn}");
+                    Console.WriteLine($"Status: {forespørgsel.Status}");
+                    Console.WriteLine();
+                }
+            }
+            
+
+            Console.Write("1. Tilbage til hovedmenuen | 2. Tilbage til forespørgselsmenuen: ");
+            if (int.TryParse(Console.ReadLine(), out int valg))
+            {
+                switch (valg)
+                {
+                    case 1:
+                        StartMenu();
+                        break;
+                    case 2:
+                        Console.Clear();    
+                        ForespoergselMenu();
+                        break;
+                    default:
+                        Fejlmeddelelse();
+                        break;
+
+                }
+            }
+            else
+            {
+                Fejlmeddelelse();
+
+            }
+            StartMenu();
+        }
+
+
+        static void RedigerEllerSletForespoergsel()
+        {
+            Console.WriteLine("Indtast forespørgselsnummer:");
+
+            if (int.TryParse(Console.ReadLine(), out int forespørgselsNr))
+            {
+                var forespørgsel = forespoergsler.Find(f => f.ForespoergselNr == forespørgselsNr);
+                if (forespørgsel == null)
+                {
+                    Console.WriteLine("Forespørgslen blev ikke fundet.");
+                    Thread.Sleep(2000);
+                    StartMenu();
+                    return;
+                }
+
+                
+                Console.WriteLine($"Rediger forespørgsel: {forespørgsel.ForespoergselNr}");
+                Console.WriteLine($"Dato: {forespørgsel.Dato:dd-MM-yyyy}");
+                Console.WriteLine($"Spil: {forespørgsel.SpilNavn}");
+                Console.WriteLine($"Kunde: {forespørgsel.Kunde.Navn}");
+                Console.WriteLine($"Telefon: {forespørgsel.Kunde.TlfNr}");
+                Console.WriteLine($"Adresse: {forespørgsel.Kunde.Adresse}");
+                Console.WriteLine($"Email: {forespørgsel.Kunde.Email}");
+                Console.WriteLine($"Nuværende status: {forespørgsel.Status}");
+                
+                Console.Write("1. Rediger forespørgsel | 2. Slet forespørgsel | 3. Tilbage: ");
+                string valg = Console.ReadLine();
+
+                switch (valg)
+                {
+                    case "1":
+                        RedigerForespoergsel(forespørgsel); 
+                        break;
+                    case "2":
+                        SletForespoergsel(forespørgselsNr);
+                        break;
+                    case "3":
+                        Console.WriteLine("Annulleret");
+                        Thread.Sleep(2000);
+                        return; 
+                    default:
+                        Fejlmeddelelse();
+                        break;
+                }
+
+            }
+
+
+        }
+
+        static void RedigerForespoergsel(Forespoergsel forespørgsel)
+
+        {
+                        
+            
+            Console.Write("Indtast ny status: 1 = Afventer, 2 = Afsluttet, 3 = Annulleret: ");
+
+            if (int.TryParse(Console.ReadLine(), out int statusNum) && Enum.IsDefined(typeof(ForespoergselsStatus), statusNum))
+            {
+                forespørgsel.Status = (ForespoergselsStatus)statusNum;
+                Console.WriteLine("Status for forespørgslen er opdateret!");
+            }
+            else
+            {
+                Console.WriteLine("Status for forespørgslen er ikke opdateret!");
+            }
+            Thread.Sleep(2000);
+            StartMenu();
+        }
+
+        static void SletForespoergsel(int forespørgselsNr)
+        {
+            var forespørgsel = forespoergsler.Find(f => f.ForespoergselNr == forespørgselsNr);
+            if (forespørgsel == null)
+            {
+                Console.WriteLine("Forespørgslen blev ikke fundet.");
+                
+            }
+            else
+            {
+                Console.WriteLine($"Rediger forespørgsel: {forespørgsel.ForespoergselNr}");
+                Console.WriteLine($"Dato: {forespørgsel.Dato:dd-MM-yyyy}");
+                Console.WriteLine($"Spil: {forespørgsel.SpilNavn}");
+                Console.WriteLine($"Kunde: {forespørgsel.Kunde.Navn}");
+                Console.WriteLine($"Telefon: {forespørgsel.Kunde.TlfNr}");
+                Console.WriteLine($"Adresse: {forespørgsel.Kunde.Adresse}");
+                Console.WriteLine($"Email: {forespørgsel.Kunde.Email}");
+                Console.WriteLine($"Nuværende status: {forespørgsel.Status}");
+
+                Console.Write("Er du sikker på at du vil slette forespørgslen? (ja/nej): ");
+                string bekræftelse = Console.ReadLine();
+
+                if (bekræftelse.ToLower() == "ja")
+                {
+                    forespoergsler.Remove(forespørgsel);
+                    Console.WriteLine("Forespørgslen blev slettet!");
+                }
+                else
+                {
+                    Console.WriteLine("Forespørgslen blev ikke slettet.");
+                }
+                
+            }
+            Thread.Sleep(2000);
+            StartMenu();
+        }
+
+
 
         static void MedarbejderLogin()
         {
@@ -214,5 +458,13 @@ namespace Genspil
             Thread.Sleep(2000);
             StartMenu();
         }
+
+        
     }
+
+
+
+
+
+    
 }
